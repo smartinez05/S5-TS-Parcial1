1d62df6f4a0b8f74bb0d845c791a93c4a972f13c
Sebastian Martinez <sebasmartinez05@outlook.com>
2021-09-01 22:23:37 -0500

    eliminado body-parser y anadido form sencillo

--- indexsebas.js
+++ indexsebas.js
@@ -1,33 +1,77 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 // parse application/json
-app.use(express.json())
+app.use(express.json());
 
-app.get('/formularioget', function (req, res) {
-  res.send('Hello World');
+app.post("/formulariopost", function (req, res) {
+	//Se envian datos de un formulario en este caso con x-www-form-urlencoded con postman y los muestra en consola ordenadamente.
+	var nombre = req.body.nombre;
+	var apellido = req.body.apellido;
+	var cedula = req.body.cedula;
+	var email = req.body.email;
+	var celular = req.body.celular;
+	var response =
+		"Nombre: " +
+		nombre +
+		"\nApellido: " +
+		apellido +
+		"\nCedula: " +
+		cedula +
+		"\nEmail: " +
+		email +
+		"\nCelular " +
+		celular;
+	console.log(response);
+	res.send("POST request");
 });
 
-app.post('/formulariopost', function (req, res) {
-  console.log(req.body.nombre);
-  console.log(req.body.apellido);
-  console.log(req.body.cedula);
-  console.log(req.body.correo);
-  console.log(req.body.celular);
-  res.send('POST request');
+app.get("/form", (req, res, next) => {
+	//Muestra un formulario para enviarlo por POST
+	res.send(`<form method="POST" action="/form">
+	<input id="nombre" name="nombre" type="text" />
+	<input id="apellido" name="apellido" type="text" />
+	<input type="number" name="cedula" id="cedula" />
+	<input type="email" name="email" id="email" />
+	<input type="celular" name="celular" id="celular" />
+	<input type="submit">
+  </form>`);
 });
 
-app.post('/formulariopost', function (req, res) {
-  console.log(req.body.nombre);
-  console.log(req.body.apellido);
-  console.log(req.body.cedula);
-  console.log(req.body.correo);
-  console.log(req.body.celular);
-  res.send('POST request');
+app.post("/form", function (req, res, next) {
+	//Recibe el formulario y muestra los datos recogidos
+	
+	res.send(
+		`<table style="border: 1px solid">
+			<tr>
+				<th>Nombre</th>
+				<th>Apellido</th>
+				<th>Cedula</th>
+				<th>Email</th>
+				<th>Celular</th>
+			</tr>
+			<tr>
+				<td>` +
+			JSON.stringify(req.body.nombre) +
+			`</td>
+				<td>` +
+			JSON.stringify(req.body.apellido) +
+			`</td>
+				<td>` +
+			JSON.stringify(req.body.cedula) +
+			`</td>
+				<td>` +
+			JSON.stringify(req.body.email) +
+			`</td>
+				<td>` +
+			JSON.stringify(req.body.celular) +
+			`</td>
+			</tr>
+		</table>`
+	);
 });
 
 app.listen(3000);
