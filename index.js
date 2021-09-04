a2e9c12f41bfa7351eaba41ebd2f406fbad45737
pablocrdozo <53062091+pablocrdozo@users.noreply.github.com>
2021-09-04 13:38:08 -0500

    Modificación archivo index.js

--- index.js
+++ index.js
@@ -25,4 +25,24 @@ app.post('/login', urlencodedParser, function (req, res) {
     res.send("Post login realizado correctamente")
 })
 
+app.post('/video', urlencodedParser, function (req, res) {
+    console.log(req.body.name)
+    console.log(req.body.weight)
+    console.log(req.body.author)
+    console.log(req.body.duration)
+    res.send("Post video realizado correctamente")
+})
+
+app.get('/form', function (req, res) {
+    res.json({
+        Nombres: 'Pablo Andrés',
+        Apellidos: 'Melo Cardozo',
+        Numero: 3224488802,
+        email: "pablocrdozo@gmail.com",
+        tipo_de_sangre: "O-",
+        eps: "famisanar",
+        edad: 20
+    });
+});
+
 app.listen(3000)
